39221fdeba771567b694a9bfeab0808b5d78b805
Shiva <Shivagautam2002@gmail.com>
2026-02-18 16:10:41 +0530

    [shiva] Add. initialized a maven project.

---
+++ app/src/main/java/com/quantitymeasurement/app/App.java
@@ -0,0 +1,10 @@
+package com.quantitymeasurement.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
